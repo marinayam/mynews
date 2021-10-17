c80787afe32b1fc106077de1a4c4728ac4432b5f
名前 <>
2021-10-17 05:08:45 +0000

    PHP/Laravel09 課題

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
 //Route::group(['prefix' => 'admin'], function(){
     //Route::get('news/create','admin\newscontroller@add');
 //});
-Route::group(['prefix' => 'XXX'], function(){
-    Route::get('XXX','AAAcontroller@bbb');
-});
+//Route::group(['prefix' => 'XXX'], function(){
+   //Route::get('XXX','AAAcontroller@bbb');
+//});
+Route::group(['prefix' => 'admin'], function(){
+    Route::get('profile/create','Admin\Profilecontroller@add');
+    Route::get('profile/edit','Admin\Profilecontroller@edit');   
+    });
