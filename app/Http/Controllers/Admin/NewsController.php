668588074c6e3dc51c2cab34c33d0ec71720e6e4
名前 <>
2021-10-17 07:55:03 +0000

    Laravel 10 課題

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -1,11 +1,11 @@
 <?php
 
-namespace App\Http\Controllers\admin;
+namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
-class newscontroller extends Controller
+class NewsController extends Controller
 {
     //
     public function add()
--- routes/web.php
+++ routes/web.php
@@ -14,13 +14,13 @@
 Route::get('/', function () {
     return view('welcome');
 });
-//Route::group(['prefix' => 'admin'], function(){
-    //Route::get('news/create','admin\newscontroller@add');
-//});
-//Route::group(['prefix' => 'XXX'], function(){
-   //Route::get('XXX','AAAcontroller@bbb');
-//});
 Route::group(['prefix' => 'admin'], function(){
-    Route::get('profile/create','Admin\Profilecontroller@add');
-    Route::get('profile/edit','Admin\Profilecontroller@edit');   
+    Route::get('news/create','Admin\NewsController@add');
+});
+Route::group(['prefix' => 'XXX'], function(){
+   Route::get('XXX','AAAcontroller@bbb');
+});
+Route::group(['prefix' => 'admin'], function(){
+    Route::get('profile/create','Admin\ProfileController@add');
+    Route::get('profile/edit','Admin\ProfileController@edit');   
     });
