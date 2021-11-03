700b7137f3e025230feb3aca96317950f51c2ecd
名前 <>
2021-11-03 17:40:12 +0900

    PHP/Laravelコース - Herokuへのデプロイ課題

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -5,6 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content ="IE = edge">
         <meta name = "viewport" content = "width=device-width,initial-scale=1">
         <title>MyProfile</title>
+        <p>プロフィール</p>
         {{-- layouts/admin.blade.phpを読み込む --}}
         @extends('layouts.admin')
         
