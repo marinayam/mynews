c590c3dec0afa63ebf0420ef55015893286e013b
名前 <>
2021-11-03 16:58:03 +0900

    PHP/Laravel 19課題提出

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -14,14 +14,10 @@ class ProfileController extends Controller
     {
         $posts = Profile::all()->sortByDesc('updated_at');
 
-        if (count($posts) > 0) {
-            $headline = $posts->shift();
-        } else {
-            $headline = null;
-        }
+
 
         // news/index.blade.php ファイルを渡している
         // また View テンプレートに headline、 posts、という変数を渡している
-        return view('profile.index', ['headline' => $headline, 'posts' => $posts]);
+        return view('profile.index', [ 'posts' => $posts]);
     }
 }
